22c84f6b31e508f3c06acee4953bef57a664b134
TestgithubAcc67 <exampleacc423@gmail.com>
2021-03-27 10:56:34 +0000

    да

--- less_1.py
+++ less_1.py
@@ -1,1 +1,38 @@
-print(10 * 20)
+s = 5
+print(s)
+
+type (3)
+
+print(type(3))
+
+print(type('Молодец'))
+
+a = ('Molodec', 1, 'Kypto', 6, 8)
+print(type(a))
+
+print(type(True))
+
+a='good' 
+b='bad'
+print(a+b)
+
+print(a * 3)
+
+c=50
+d=10
+print(f'вставим числа {c} и {d}')
+
+a = [1, 3, 6]
+print(a[-3])
+
+b=[2, 10, 35]
+print(a+b)
+
+
+
+s=[]
+s.append(10)
+s.append(20)
+print(s)
+
+print(len(s))
